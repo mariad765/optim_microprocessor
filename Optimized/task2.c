4ad56d7d6c4e1ea4667a4cdf2b3595c71db8452c
mariad765 <maria.adina.d@gmail.com>
2023-05-22 05:22:50 -0400

    Created new functions so not all code is in main like it used to

--- Optimized/task2.c
+++ Optimized/task2.c
@@ -1,71 +1,76 @@
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 #define NR_MAX_numere 17
 
-int main() {
+// Function to read the instruction from the user
+unsigned int readInstruction() {
+  unsigned int inst;
+  scanf("%u", &inst);
+  return inst;
+}
 
-  unsigned int inst;  // instruction
-  scanf("%u", &inst); // read instruction
-  int N;              // number of instructions
-  int op;             // var for deciding operators
-  // Vector for storing numbers to be decomposed for operands.
-  // Dynamically allocate memory for numere.
-  unsigned short *numere = malloc(NR_MAX_numere * sizeof(unsigned short));
+// Function to determine the number of instructions
+int getNumInstructions(unsigned int inst) {
+  int N = inst >> (32 - 3);
+  return N + 1;
+}
 
-  // Dynamically allocate memory for vect_operatori.
-  char *vect_operatori = malloc(NR_MAX_numere * sizeof(char));
+// Function to determine the operator based on the instruction bits
+char getOperator(unsigned int op) {
+  op = op >> 30;
+  char operator;
+  if (op == 0)
+    operator= '+';
+  else if (op == 1)
+    operator= '-';
+  else if (op == -2)
+    operator= '*';
+  else if (op == -1)
+    operator= '/';
+  return operator;
+}
 
-  N = inst >> (32 - 3); // shift bits
-  N = N + 1;            // Relevant number for the first 3 bits.
-
-  for (int i = 0; i < N; i++) {
-    op = inst << (3 + 2 * i); // Shifting to select bits in pairs of 2.
-    op = op >> 30;
-    if (op == 0) // Conditions for deciding the operation.
-    {
-
-      vect_operatori[i] = '+';
-    }
-
-    if (op == 1) {
-      vect_operatori[i] = '-';
-    }
-
-    if (op == -2) {
-      vect_operatori[i] = '*';
-    }
-    if (op == -1) {
-      vect_operatori[i] = '/';
-    }
-  }
-
-  unsigned int Dim = 0; // dimension
-  Dim = inst << (3 + 2 * N);
+// Function to calculate the dimension
+int getDimension(unsigned int inst, int N) {
+  int Dim = inst << (3 + 2 * N);
   Dim = Dim >> 28;
-  Dim++;
+  return Dim + 1;
+}
 
-  int x;
-  x = 16 / Dim;      // number of operands.
-  int nr_numere = 0; // numbers that need to be decomposed to find the operands.
-  unsigned short operand *= calloc(NR_MAX_numere, sizeof(unsigned short));
-  if (((N + 1) * Dim) % 16 == 0) {
-    nr_numere = ((N + 1) * Dim) / 16;
-  } else if (((N + 1) * Dim) % 16 != 0) {
-    nr_numere = (((N + 1) * Dim) / 16) + 1;
-  }
-  long int rezult = 0;
+// Function to calculate the number of operands
+int getNumOperands(int Dim) { return 16 / Dim; }
 
-  int k = 0;
+// Function to calculate the number of numbers that need to be decomposed
+int getNumNumbers(int N, int Dim) {
+  int nr_numere = ((N + 1) * Dim) / 16;
+  if (((N + 1) * Dim) % 16 != 0)
+    nr_numere++;
+  return nr_numere;
+}
+
+// Function to read numbers from the user
+void readNumbers(unsigned short *numere, int nr_numere) {
   for (int i = 0; i < nr_numere; i++) {
     scanf("%hu", &numere[i]);
+  }
+}
+
+// Function to perform the calculations
+long int performCalculations(unsigned short *numere, char *vect_operatori,
+                             int nr_numere, int Dim, int x) {
+  unsigned short *operand = calloc(NR_MAX_numere, sizeof(unsigned short));
+  long int rezult = 0;
+  int k = 0;
+  for (int i = 0; i < nr_numere; i++) {
     for (int j = 0; j < x; j++) {
       operand[j] = numere[i] << (Dim * j);
       operand[j] = operand[j] >> (Dim * (x - 1));
-      if ((i == 0) && (j == 0)) {
+      if ((i == 0) && (j == 0))
         rezult = operand[0];
-      } else {
+      else {
         rezult = rezult;
-
         switch (vect_operatori[k]) {
         case '+':
           rezult = operand[j] + rezult;
@@ -80,15 +85,27 @@ int main() {
           rezult = rezult / operand[j];
           break;
         }
-
         k++;
       }
     }
   }
+  free(operand);
+  return rezult;
+}
 
+int main() {
+  unsigned int inst = readInstruction();
+  int N = getNumInstructions(inst);
+  unsigned short *numere = malloc(NR_MAX_numere * sizeof(unsigned short));
+  char *vect_operatori = malloc(NR_MAX_numere * sizeof(char));
+  int Dim = getDimension(inst, N);
+  int x = getNumOperands(Dim);
+  int nr_numere = getNumNumbers(N, Dim);
+  readNumbers(numere, nr_numere);
+  long int rezult =
+      performCalculations(numere, vect_operatori, nr_numere, Dim, x);
   printf("%ld\n", rezult);
   free(vect_operatori);
   free(numere);
-  free(operand);
   return 0;
 }
