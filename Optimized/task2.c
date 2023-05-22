3876be0172368bcdeb6fa5c020066d4704763bb6
mariad765 <maria.adina.d@gmail.com>
2023-05-22 05:08:35 -0400

    Fix issue with file handling

--- Optimized/task2.c
+++ Optimized/task2.c
@@ -1,110 +1,94 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 #define NR_MAX_numere 17
 
-int main()
-{
+int main() {
 
-    unsigned int inst;                    // instructiunea
-    scanf("%u", &inst);                   // se citeste instructiunea
-    int N;                                // numarul de instructiuni
-    int op;                               // variabila pentru decidere operatori
-    unsigned short numere[NR_MAX_numere]; // vector stocare numere de descompus pt operanzi
-    char vect_operatori[NR_MAX_numere];   // vector n care retinem operatorii
+  unsigned int inst;  // instruction
+  scanf("%u", &inst); // read instruction
+  int N;              // number of instructions
+  int op;             // var for deciding operators
+  // Vector for storing numbers to be decomposed for operands.
+  // Dynamically allocate memory for numere.
+  unsigned short *numere = malloc(NR_MAX_numere * sizeof(unsigned short));
 
-    N = inst >> (32 - 3); // shifteaza bitii
-    N = N + 1;            // numarul relevant pt primii 3 biti
-   // printf("%d", N);      // afiseaza numarul de op
+  // Dynamically allocate memory for vect_operatori.
+  char *vect_operatori = malloc(NR_MAX_numere * sizeof(char));
 
-    for (int i = 0; i < N; i++)
+  N = inst >> (32 - 3); // shift bits
+  N = N + 1;            // Relevant number for the first 3 bits.
+
+  for (int i = 0; i < N; i++) {
+    op = inst << (3 + 2 * i); // Shifting to select bits in pairs of 2.
+    op = op >> 30;
+    if (op == 0) // Conditions for deciding the operation.
     {
-        op = inst << (3 + 2 * i); // shiftare pentru a selecta bitii 2 cate 2
-        op = op >> 30;
-        if (op == 0) // conditiile pentru decidere op
-        {
-            //printf(" +");
-            vect_operatori[i] = '+';
-        }
 
-        if (op == 1)
-        {
-            vect_operatori[i] = '-';
-           // printf(" -");
-        }
-
-        if (op == -2)
-        {
-            vect_operatori[i] = '*';
-          //  printf(" *");
-        }
-        if (op == -1)
-        {
-            vect_operatori[i] = '/';
-           // printf(" /");
-        }
+      vect_operatori[i] = '+';
     }
 
-    unsigned int Dim = 0; // dimensiunea
-    Dim = inst << (3 + 2 * N);
-    Dim = Dim >> 28;
-    Dim++;
-    // printf(" %u\n", Dim);
-    // cod task 2
-    int x;
-    x = 16 / Dim;      // numarul de operanzi
-    int nr_numere = 0; // numerele care trebuie descompuse pt aflare operanzi
-    unsigned short operand[NR_MAX_numere];
-    if (((N + 1) * Dim) % 16 == 0)
-    {
-        nr_numere = ((N + 1) * Dim) / 16;
+    if (op == 1) {
+      vect_operatori[i] = '-';
     }
-    else if (((N + 1) * Dim) % 16 != 0)
-    {
-        nr_numere = (((N + 1) * Dim) / 16) + 1;
+
+    if (op == -2) {
+      vect_operatori[i] = '*';
     }
-    long int rezult = 0;
-    // printf("Operand 0:%hu\n", rezult);
-    int k = 0;
-    for (int i = 0; i < nr_numere; i++)
-    {
-        scanf("%hu", &numere[i]);
-        for (int j = 0; j < x; j++)
-        {
-            operand[j] = numere[i] << (Dim * j);
-            operand[j] = operand[j] >> (Dim * (x - 1));
-            if ((i == 0) && (j == 0))
-            {
-                rezult = operand[0];
-            }
-            else
-               { rezult = rezult;
+    if (op == -1) {
+      vect_operatori[i] = '/';
+    }
+  }
 
-            switch (vect_operatori[k])
-            {
-            case '+':
-                rezult = operand[j] + rezult;
-                break;
-            case '-':
-                rezult = rezult - operand[j];
-                break;
-            case '*':
-                rezult = rezult * operand[j];
-                break;
-            case '/':
-                rezult = rezult / operand[j];
-                break;
-            }
-            
-              k++;
-               }
-               // printf("\nrezultat=%ld\n", rezult);
-          
+  unsigned int Dim = 0; // dimension
+  Dim = inst << (3 + 2 * N);
+  Dim = Dim >> 28;
+  Dim++;
 
-          //  printf("OPERAND: %hu\n ", operand[j]); // afisam operanzii
+  int x;
+  x = 16 / Dim;      // number of operands.
+  int nr_numere = 0; // numbers that need to be decomposed to find the operands.
+  unsigned short operand *= calloc(NR_MAX_numere, sizeof(unsigned short));
+  if (((N + 1) * Dim) % 16 == 0) {
+    nr_numere = ((N + 1) * Dim) / 16;
+  } else if (((N + 1) * Dim) % 16 != 0) {
+    nr_numere = (((N + 1) * Dim) / 16) + 1;
+  }
+  long int rezult = 0;
+
+  int k = 0;
+  for (int i = 0; i < nr_numere; i++) {
+    scanf("%hu", &numere[i]);
+    for (int j = 0; j < x; j++) {
+      operand[j] = numere[i] << (Dim * j);
+      operand[j] = operand[j] >> (Dim * (x - 1));
+      if ((i == 0) && (j == 0)) {
+        rezult = operand[0];
+      } else {
+        rezult = rezult;
+
+        switch (vect_operatori[k]) {
+        case '+':
+          rezult = operand[j] + rezult;
+          break;
+        case '-':
+          rezult = rezult - operand[j];
+          break;
+        case '*':
+          rezult = rezult * operand[j];
+          break;
+        case '/':
+          rezult = rezult / operand[j];
+          break;
         }
-    }
-   
-    printf("%ld\n", rezult);
 
-    return 0;
+        k++;
+      }
+    }
+  }
+
+  printf("%ld\n", rezult);
+  free(vect_operatori);
+  free(numere);
+  free(operand);
+  return 0;
 }
