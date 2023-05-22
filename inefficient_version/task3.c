c818a664e9186821c2b1ac16a528c473f090ff2f
AlexChiorean <110402179+AlexChiorean@users.noreply.github.com>
2023-05-22 10:24:19 +0300

    Update task3.c
    eliminating useless comments

--- inefficient_version/task3.c
+++ inefficient_version/task3.c
@@ -50,37 +50,27 @@ int main() {
         binary[i] = (int)(inst % (unsigned int)two);
         inst /= (unsigned int)two;
     }
-    // for (i = 31; i >= 0; i--)
-        // printf("%c", binary[i]);
-    // printf("\n");
     N = four * binary[thirtyone] + two * binary[thirty] + binary[twentynine] + one;
-    // printf("%d ", N);
     for (i = zero; i < N; i++) {
         if (binary[twentyeight - two * i] == zero && binary[twentyseven - two * i] == zero) {
-            // printf("+ ");
             operations[m++] = '+';
         }
         if (binary[twentyeight - two * i] == zero && binary[twentyseven - two * i] == one) {
-            // printf("- ");
             operations[m++] = '-';
         }
         if (binary[twentyeight - two * i] == one && binary[twentyseven - two * i] == zero) {
-            // printf("* ");
             operations[m++] = '*';
         }
         if (binary[twentyeight - two * i] == one && binary[twentyseven - two * i] == one) {
-            // printf("/ ");
             operations[m++] = '/';
         }
     }
     operations[m] = zero;
     dim = eight * (binary[twentyeight - two * i]) + four * (binary[twentyseven - two * i])
     + two * (binary[twentysix - two * i]) + (binary[twentyfive - two * i]) + one;
-    // printf("%d\n", dim);
     int NumOfInts = zero;
     NumOfInts = ((N + one) * dim) / sixteen;
     if (((N + one) * dim) % sixteen) NumOfInts++;
-    // printf("Numere de introdus = %d\nIntroduceti numar: ", NumOfInts);
     int binaryOperand[ten * sixteen] = {zero};
     for (i = zero; i < NumOfInts; i++) {
         scanf("%u", &inst);
@@ -89,32 +79,13 @@ int main() {
             inst /= (unsigned int)two;
         }
     }
-    /**
-    for (i = 0; i < NumOfInts; i++) {
-        for (int j = 0; j < 16 / dim; j++) {
-            for (int k = 0; k < dim; k++)
-                printf("%d", binaryOperand[i][15 - dim * j - k]);
-            printf(" ");
-        }
-        printf("\n");
-    }*/
-    // for (i = zero; i < NumOfInts * sixteen; i++)printf("%d", binaryOperand[i]);
-    // printf("\n");
     int operand[twentyone] = {zero}, q = zero;
     for (i = zero; i <= N; i++) {
         for (int j = zero; j < dim; j++) {
-            // printf("%d", binaryOperand[i * sixteen + dim - j - 1]);
             operand[q] += topowof(two, dim - one - j) * binaryOperand[i * dim + j];
         }
-        // printf(" ");
         q++;
     }
-    /*
-    printf("Operanzii: ");
-    for (i = 0; i < q; i++)
-        printf("%d ", operand[i]);
-    printf("\n");
-    */
     for (i = zero; i < N; i++) {
         if (operations[i] == '+') operand[i + one] += operand[i];
         if (operations[i] == '-') operand[i + one] = operand[i] - operand[i + one];
